32b0fe6d488f1394f3e7ad6089bdcf810d708d71
Devon Hackley <devonhackley@gmail.com>
2019-05-15 15:45:10 -0700

    render list of fortunes

--- src/components/history.js
+++ src/components/history.js
@@ -14,17 +14,34 @@ class History extends React.Component {
   componentDidMount() {
     if(this.props.getUrl === 'historyPage') {
       superagent.get(`${process.env.REACT_APP_API_URL}/fortunes`).query({data: { username: this.props.user}})
-      .then(results => this.setState({ fortunes: results }))
+      .then(results => this.setState({ fortunes: results.body }))
       .catch(err => console.error(err));
     }
   }
 
   render(){
-    console.log('props', this.props);
+    const { fortunes } = this.state;
+    const content = (
+      fortunes.map((val, idx) => {
+        console.log('type', typeof val.created_on);
+        let date = new Date(parseInt(val.created_on)).toDateString();
+        return (
+          <li key={idx}>
+                  <p>{`Created on: ${date}`}</p>
+                  <p>{val.fortune}</p>
+          </li>
+        );
+      })
+    )
     return(
       <>
         <NavBar setURL={this.props.setURL} getUrl={this.props.getUrl} user={this.props.user}/>
         <h2>History</h2>
+        <section id="history-container">
+          <ul>
+            {content}
+          </ul>
+        </section>
       </>
     )
   }
