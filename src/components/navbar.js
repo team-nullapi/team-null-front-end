faa590193736fcc3d84f94e9937cd0a12512d408
Devon Hackley <devonhackley@gmail.com>
2019-05-16 08:38:39 -0700

    added some css

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -39,7 +39,7 @@ class ButtonAppBar extends React.Component {
   render(){
     const { classes } = this.props;
     return (
-      <div className={classes.root}>
+      <div id="page-nav" className={classes.root}>
         <AppBar position="static"  style={{ background: 'transparent', boxShadow: 'none'}}>
           <Toolbar>
             <IconButton onClick={this.handleHomeClick} className={classes.menuButton} color="inherit" aria-label="Menu">
@@ -47,7 +47,7 @@ class ButtonAppBar extends React.Component {
                 <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
               </SvgIcon>
             </IconButton>
-            <Typography variant="h6" color="inherit" className={classes.grow}>
+            <Typography id="title" variant="h6" color="inherit" className={classes.grow}>
                 Face Your Fortune
             </Typography>
             {(this.props.getUrl === 'startPage') || (this.props.getUrl === 'historyPage') ? ' ' : <Button onClick={this.handlePollClick} color="inherit"> <i class="material-icons">poll</i></Button> }
